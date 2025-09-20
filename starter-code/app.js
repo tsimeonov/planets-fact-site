8d3cc0f7e1a619100f1666f2ab9bb63c58130ad4
tsimeonov <tsimeonov12@gmail.com>
2025-09-20 15:29:44 +0200

    update

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,7 +5,6 @@ const header = document.querySelector('.header');
 const toggleNav = () => {
 	navbar.classList.toggle('active');
 	mobileNav.classList.toggle('hamburger-active');
-	// Toggle a class on the header insted of directly changing the style
 	header.classList.toggle('header-border-active');
 };
 mobileNav.addEventListener('click', () => toggleNav());
