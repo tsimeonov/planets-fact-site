69009f142654feb6914a67030bea61b18f5d7011
tsimeonov <tsimeonov12@gmail.com>
2025-09-20 14:47:04 +0200

    update

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,8 +1,11 @@
 const mobileNav = document.querySelector('.hamburger');
 const navbar = document.querySelector('.menubar');
+const header = document.querySelector('.header');
 
 const toggleNav = () => {
 	navbar.classList.toggle('active');
 	mobileNav.classList.toggle('hamburger-active');
+	// Toggle a class on the header insted of directly changing the style
+	header.classList.toggle('header-border-active');
 };
 mobileNav.addEventListener('click', () => toggleNav());
