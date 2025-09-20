f05f00f89d9571d37020cc82ce367a60c4ce299e
tsimeonov <tsimeonov12@gmail.com>
2025-09-20 15:42:19 +0200

    add spans

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,10 @@
 const mobileNav = document.querySelector('.hamburger');
 const navbar = document.querySelector('.menubar');
-const header = document.querySelector('.header');
+// const header = document.querySelector('.header');
 
 const toggleNav = () => {
 	navbar.classList.toggle('active');
 	mobileNav.classList.toggle('hamburger-active');
-	header.classList.toggle('header-border-active');
+	// header.classList.toggle('header-border-active');
 };
 mobileNav.addEventListener('click', () => toggleNav());
