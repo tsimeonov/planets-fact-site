ac80a345d5043f788ecc5022b9098788acb9de51
tsimeonov <tsimeonov12@gmail.com>
2025-09-27 14:26:08 +0200

    Update app.js

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,6 +1,7 @@
 // DOM element selection
 const planetLinks = document.querySelectorAll('nav ul li a, .menubar ul li a');
 const planetImage = document.querySelector('.image img');
+const planetName = document.querySelector('.info h1');
 
 const mobileNav = document.querySelector('.hamburger');
 const navbar = document.querySelector('.menubar');
